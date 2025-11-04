1b34b03058b34324598d37d2ca24e65c42872f38
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-04 21:50:04 +0300

    added test for function src.utils.get_greeting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,28 @@
+from freezegun import freeze_time
+
+import src.utils
 from src.utils import get_month_period
 
 
 def test_get_month_period() -> None:
     assert get_month_period("2021-12-30 08:16:00") == ["01.12.2021 08:16:00", "30.12.2021 08:16:00"]
+
+
+@freeze_time("2025-01-01 12:10:20")
+def test_get_greeting_afternoon() -> None:
+    assert src.utils.get_greeting() == "Добрый день"
+
+
+@freeze_time("2025-01-01 09:25:12")
+def test_get_greeting_morning() -> None:
+    assert src.utils.get_greeting() == "Доброе утро"
+
+
+@freeze_time("2025-01-01 17:35:12")
+def test_get_greeting_evening() -> None:
+    assert src.utils.get_greeting() == "Добрый вечер"
+
+
+@freeze_time("2025-01-01 23:35:12")
+def test_get_greeting_night() -> None:
+    assert src.utils.get_greeting() == "Доброй ночи"
