c13d3638cc986c4bc698e675c75dd03de322510f
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-06 00:07:26 +0300

    added spending_by_workday

--- src/reports.py
+++ src/reports.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 from functools import wraps
+from json import JSONDecodeError
 from pathlib import Path
 from typing import Any
 from typing import Callable
@@ -64,16 +65,21 @@ def filtered_by_date(df: pd.DataFrame, date: Optional[str] = None) -> pd.DataFra
     :param date: опциональную дату в формате 'dd.mm.YYYY'
     :return: транзакции за последние три месяца (от переданной даты)
     """
-    df["Дата платежа"] = pd.to_datetime(df["Дата платежа"], format="%d.%m.%Y")
-    date_format = "%d.%m.%Y"
-    if not date:
-        date = datetime.datetime.now().strftime(date_format)
-    dt = datetime.datetime.strptime(date, date_format)
-    logger.info(f"получение даты: {dt}")
-    past_datetime = dt - relativedelta(months=3)
-    filtered_by_date = df[(df["Дата платежа"] >= past_datetime) & (df["Дата платежа"] <= dt)]
+    try:
+        logger.info("фильтрация транзакции за последние три месяца")
+        df["Дата платежа"] = pd.to_datetime(df["Дата платежа"], format="%d.%m.%Y")
+        date_format = "%d.%m.%Y"
+        if not date:
+            date = datetime.datetime.now().strftime(date_format)
+        dt = datetime.datetime.strptime(date, date_format)
+        logger.info(f"получение даты: {dt}")
+        past_datetime = dt - relativedelta(months=3)
+        filtered_by_date = df[(df["Дата платежа"] >= past_datetime) & (df["Дата платежа"] <= dt)]
 
-    return filtered_by_date
+        return filtered_by_date
+    except (KeyError, TypeError, AssertionError) as ex:
+        logger.error(f"Ошибка получение транзакции за последние три месяца : {ex}")
+        return pd.DataFrame()
 
 
 def spending_by_category(df: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
@@ -99,7 +105,7 @@ def spending_by_category(df: pd.DataFrame, category: str, date: Optional[str] = 
 
 def spending_by_weekday(df: pd.DataFrame, date: Optional[str] = None) -> str:
     """
-     Функция принимает датафрейм с транзакциями,  дату.
+    Функция принимает датафрейм с транзакциями,  дату.
     Функция возвращает средние траты в каждый из дней недели за последние три месяца (от переданной даты)
     Если дата не передана, то берется текущая дата.
 
@@ -107,18 +113,44 @@ def spending_by_weekday(df: pd.DataFrame, date: Optional[str] = None) -> str:
     :param date:опциональную дату в формате 'dd.mm.YYYY'
     :return: Функция возвращает средние траты в каждый из дней недели за последние три месяца (от переданной даты)
     """
-    filter_df = filtered_by_date(df, date)
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    data = []
-    for weekday in weekdays:
-        filter_by_weekday = filter_df[filter_df["Дата платежа"].dt.day_name() == weekday]
-        avg_amount = filter_by_weekday["Сумма платежа"].mean()
+    try:
+        filter_df = filtered_by_date(df, date)
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        data = []
+        for weekday in weekdays:
+            filter_by_weekday = filter_df[filter_df["Дата платежа"].dt.day_name() == weekday]
+            avg_amount = filter_by_weekday["Сумма платежа"].mean()
+            if avg_amount:
+                avg_amount = 0.00
+            data.append({weekday: round(float(avg_amount), 2)})
+        logger.info(f"получение средние траты в каждый из дней недели за последние три месяца: {data}")
+        return json.dumps(data, ensure_ascii=False, indent=4)
+    except (KeyError, JSONDecodeError, TypeError, AssertionError) as ex:
+        logger.error(f"Ошибка получение средние траты в каждый из дней недели за последние три месяца : {ex}")
+        return ""
 
-        data.append({weekday: round(float(avg_amount), 2)})
-    return json.dumps(data, ensure_ascii=False, indent=4)
+
+def spending_by_workday(df: pd.DataFrame, date: Optional[str] = None) -> str:
+    """
+    Функция принимает датафрейм с транзакциями,  дату.
+    Функция выводит средние траты в рабочий и в выходной день за последние три месяца (от переданной даты).
+    Если дата не передана, то берется текущая дата.
+
+    :param df:датафрейм с транзакциями
+    :param date:опциональную дату в формате 'dd.mm.YYYY'
+    :return: Функция выводит средние траты в рабочий и в выходной день за последние три месяца (от переданной даты).
+    """
+    weekday_spending = spending_by_weekday(df, date)
+    if weekday_spending:
+        weekday_data = json.loads(weekday_spending)
+        result = {"рабочий день": weekday_data[:5], "выходной день": weekday_data[5:]}
+        logger.info(f"средние траты в рабочий и в выходной день за последние три месяца(от переданной даты): {result}")
+        return json.dumps(result, ensure_ascii=False, indent=4)
+    logger.error("Ошибка получение  средние траты в рабочий и в выходной день за последние три месяца")
+    return ""
 
 
 if __name__ == "__main__":
     dtf = read_excel("operations")
     # print(spending_by_category(dtf,"Супермаркеты",'21.03.2021' ))
-    print(spending_by_weekday(dtf, "21.03.2021"))
+    print(spending_by_workday(dtf, "21.03.2026"))
